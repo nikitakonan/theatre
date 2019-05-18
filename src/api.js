13c93b633598a56d5dda25a0d16430d61a05f706
nikitakonan <nikitakonan1991@gmail.com>
2019-05-18 11:24:52 +0300

    Check api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
 import actors from './actors';
 
 export function getActors() {
+    axios.get('/api/actors')
+        .then(response => {
+            console.log(response);
+        });
     return new Promise(resolve => {
         setTimeout(() => resolve(actors), 1000);
     });
